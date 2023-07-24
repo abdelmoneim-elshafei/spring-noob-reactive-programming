9653bc8c854ed92db423e186354c87d3470728b3
abdelmoneim <abdelmoneimelshafei@proton.me>
2023-07-24 22:34:51 +0300

    first commit

---
+++ src/test/java/noob/reactive/NoobReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package noob.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoobReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
